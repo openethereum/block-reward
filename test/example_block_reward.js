2a6bde29a80081f037b96e14d2ef0fecbd2779e7
André Silva <andre.beat@gmail.com>
2018-06-12 15:28:19 +0100

    fix tests and increase coverage

--- test/example_block_reward.js
+++ test/example_block_reward.js
@@ -3,15 +3,56 @@
 const ExampleBlockReward = artifacts.require("./ExampleBlockReward.sol");
 
 contract("ExampleBlockReward", accounts => {
-  // since we can't do system calls with truffle this is the only thing we can test
-  it("should reject calls not coming from system", async () => {
-    const block_reward = await ExampleBlockReward.new();
+  const SYSTEM = accounts[9];
 
-    let benefactors = [1, 2, 3, 4];
-    let kind = [0, 1, 2, 3];
+  it("should calculate rewards", async () => {
+    const blockReward = await ExampleBlockReward.new(SYSTEM);
+
+    const benefactors = [
+      accounts[0],
+      accounts[1],
+      accounts[2],
+      accounts[3],
+    ];
+
+    const kind = [0, 1, 2, 3];
+
+    // we do a local call so we can get back the return value
+    const rewards = await blockReward.reward.call(benefactors, kind, { from: SYSTEM });
+
+    // request with transaction should not fail
+    await blockReward.reward(benefactors, kind, { from: SYSTEM });
+
+    const expectedRewards = [1000, 1001, 1002, 1003];
+
+    assert.deepEqual(rewards[0], benefactors);
+    assert.deepEqual(rewards[1].map(v => v.toNumber()), expectedRewards);
+  });
+
+  it("should reject calls not coming from system", async () => {
+    const blockReward = await ExampleBlockReward.new(SYSTEM);
+
+    const benefactors = [1, 2, 3, 4];
+    const kind = [0, 1, 2, 3];
 
     try {
-      await block_reward.reward(benefactors, kind);
+      await blockReward.reward(benefactors, kind);
+    } catch (err) {
+      assert(err.message.includes("revert"));
+      return;
+    }
+
+    assert.fail("Expected fn to throw");
+  });
+
+  it("should validate benefactors and kinds have same size", async () => {
+    const blockReward = await ExampleBlockReward.new(SYSTEM);
+
+    const benefactors = [1, 2, 3];
+    const kind = [0, 1, 2, 3];
+
+    try {
+      await blockReward.reward(benefactors, kind, { from: SYSTEM });
     } catch (err) {
       assert(err.message.includes("revert"));
       return;
